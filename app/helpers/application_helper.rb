96c60c625c57a5208544f05fa0012e46594977d9
s20a3264 <s20a3264@hotmail.com>
2015-08-27 01:32:13 +0800

    user_nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+
+	def	render_navbar
+		if logged_in?
+			render 'layouts/user_nav'
+		else
+			render 'layouts/not_login_nav'
+		end		
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,4 +8,8 @@ module SessionsHelper
 		@current_user ||= User.find_by(id: session[:user_id])
 	end
 
+	def logged_in?
+		current_user.present?
+	end
+
 end
