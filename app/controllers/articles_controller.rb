a553d9ce2f52b3113006a15ed27a946694e53fcb
s20a3264 <s20a3264@hotmail.com>
2015-09-02 04:09:13 +0800

    修改css和佈局

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,16 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@articles = current_user.articles.all
+	end
+
 	def new
 		@article = current_user.articles.build
+	end
+
+	def show
+		@user = User.find(params[:user_id])
+		@article = @user.articles.find(params[:id])
 	end
 
 	def create
@@ -14,6 +23,35 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def edit
+		@article = current_user.articles.find(params[:id])
+	end
+
+	def update
+		@article = current_user.articles.find(params[:id])
+		if @article.update_attributes(article_params)
+			flash[:success] = "編輯成功"
+			redirect_to user_path(current_user)
+		else
+			render :edit
+		end	
+	end
+
+	def destroy
+		@article = current_user.articles.find(params[:id])
+		if @article.destroy
+			flash[:success] = "文章已刪除"
+			redirect_to user_articles_path(current_user)
+		else
+			flash[:danger] = "刪除失敗"
+			redirect_to user_articles_path(current_user)
+		end		
+	end
+
+
+	def url_options
+		{ user_id: current_user }.merge(super)
+	end
 	
 
 	private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@articles = @user.articles.page(params[:page]).per(2)
+		@articles = @user.articles.page(params[:page]).per(3)
 	end
 
 
