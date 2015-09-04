6195504744f2348d83cf258ceb0bca5e53be43aa
s20a3264 <s20a3264@hotmail.com>
2015-09-04 19:09:45 +0800

    for heroku

---
+++ app/helpers/nav_helper.rb
@@ -0,0 +1,45 @@
+module NavHelper
+
+	def	render_navbar
+		render logged_in? ? 'layouts/user_nav' : 
+	  							      'layouts/guest_nav' 
+  end
+
+  def render_dropdown_nav
+  	render @user == current_user ? 'shared/dropdown_icon' : 
+  																 'shared/dropdown_gravatar'
+  end
+
+  def user_blog_title
+    if  @user == current_user
+    	title = "#{current_user.name}'s Blog"
+    	path = user_path(current_user)
+    else
+    	title = "#{@user.name}'s Blog"
+    	path = user_path(@user)
+    end	
+    return link_to title, path, id: "logo"
+  end
+
+  def guest_blog_title
+  	if @user.nil?
+  		title =	"$&!$#%#$%"
+  	  path = root_path
+  	else 
+  		if params[:id]  
+  			title = "#{@user.name}'s Blog"
+    		path = user_path(@user)
+      elsif params[:user_id]
+				title = "#{@user.name}'s Blog"
+    		path = user_path(@user)
+      else	
+        title =	"$&!$#%#$%"
+  	    path = root_path
+  	  end  
+  	end
+  	return link_to title, path, id: "logo"
+  end
+
+
+
+end
